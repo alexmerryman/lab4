fa6e5f85d292fd10e8567d6bf0aef65bb481ae80
alexmerryman <alexandermerryman2018@u.northwestern.edu>
2016-01-27 13:52:57 -0600

    v1.0

--- src/lab4.cpp
+++ src/lab4.cpp
@@ -1,24 +1,36 @@
 #include "eecs230.h"
 
-struct Date {
-    int year;
-    int month;
-    int day;
-    bool isAD;
-    Date(int y, int m, int d, bool AD)
-    {
-        year = y;
-        month = m;
-        day = d;
-        isAD = AD;
-    };
+struct point {
+    double x;
+    double y;
+};
+
+struct LineSegment {
+    point pt1;
+    point pt2;
+};
+
+LineSegment addition(point pt1, point pt2) {
+    double x_coord_add = pt1.x + pt2.x;
+    double y_coord_add = pt1.y + pt2.y;
+    LineSegment VecAdd(double x_coord_add, double y_coord_add);
+};
+
+LineSegment subtraction(point pt1, point pt2) {
+    double x_coord_sub = pt1.x - pt2.x;
+    double y_coord_sub = pt1.y - pt2.y;
+    LineSegment VecSub(double x_coord_sub, double y_coord_sub);
 };
 
 int main()
 try
 {
-    Date today(2016,1,27,true);
-    cout << today.day;
+    point pt1;
+    point pt2;
+    pt1.x = 0;
+    pt1.y = 0;
+    pt2.x = 1;
+    pt2.y = 1;
 }
 
 catch (runtime_error& e) {
